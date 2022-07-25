748338d33722731e17603150edd1ea91bda6c100
Randy Davis <randy_davis@outlook.com>
2022-07-25 01:56:15 -0500

    Type Inference and the Any type

--- 03-TypeAnnotationBasics/Types/variables.ts
+++ 03-TypeAnnotationBasics/Types/variables.ts
@@ -10,3 +10,28 @@ numCatLivs = 'zero' // Complains bc numCatLives is a number and we're trying to 
 let gameOver: boolean = false
 gameOver = true
 gameOver = 'true' // Complains bc gameOver is a boolean and we're trying to assign a string
+
+// Type Inference
+let tvShow = 'The Wire'
+tvShow = 'The Simpsons'
+tvShow = false
+
+let isFunny = false
+isFunny = true
+isFunny = 'asdf' // Complains bc isFunny is a boolean and we're trying to assign a string
+
+// The any type
+let thing: any = 'hello'
+thing = 5
+thing = true
+thing()
+thing.toUpperCase()
+
+const movies = ['The Arrival', 'The Thing', 'Aliens']
+let foundMovie: string
+
+for (let movie of movies) {
+  if (movie === 'The Thing') {
+    foundMovie = 'The Thing'
+  }
+}
