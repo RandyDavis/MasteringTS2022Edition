c4b007305a555fe01fbc67fb25bc8ae4f48e7014
Randy Davis <randy.davis@extend.com>
2022-07-25 11:05:07 -0500

    Working with Default Parameters

--- 04-Functions/functions.ts
+++ 04-Functions/functions.ts
@@ -2,12 +2,16 @@ function square(num: number) {
   return num * num
 }
 
-function greet(person: string) {
-  return `Hello, ${person}!`
-}
+// function greet(person: string) {
+//   return `Hello, ${person}!`
+// }
 
 const doSomething = (person: string, age: number, isFunny: boolean) => {
   return `${person} is ${age} and is ${isFunny ? 'funny' : 'not funny'}.`
+}
+
+function greet(person: string = 'Randy') {
+  return `Hello, ${person}!`
 }
 
 square(3)
